d06ae4ee0abd0ca946a364abc481e8a7f7c5f7a8
Federico Aniello <aniello@edilportale.com>
2023-10-29 00:22:08 +0200

    schema

--- src/app/api/products/schema.ts
+++ src/app/api/products/schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const productSchema = z.object({
-    id: z.number(),
+    id: z.number().optional(),
     name: z.string(),
     price: z.number().nonnegative(),
 });
